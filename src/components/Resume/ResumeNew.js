808f1f2ab10f71df8bf620210cd52669c63fe1b1
Ichrak Hamdi <ichrak.hamdi@ensi-uma.tn>
2023-10-11 14:49:59 +0100

    updated2

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -25,9 +25,10 @@ function ResumeNew() {
        
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+        <Document file={resumeLink} className="d-flex justify-content-center">
+  <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+  <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+</Document>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
